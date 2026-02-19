d2b324c86b912c1290741dd4c418a128b38d8676
krupalbandari226-hu <krupalbandari226@gmail.com>
2026-02-19 10:53:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ http.createServer((req, res) => {
 res.write("Hello from DEV Branch - NodeJS App");
 res.end();
 }).listen(3000);
+{
+  "name": "node-app",
+  "version": "1.0.0",
+  "main": "index.js"
+}
+FROM node:18
+WORKDIR /app
+COPY . .
+EXPOSE 3000
+CMD ["node", "index.js"]
+
 
