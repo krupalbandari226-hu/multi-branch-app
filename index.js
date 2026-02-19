fbc8359ce649c6a6c43ed4136f843b7679ca7cb0
krupalbandari226-hu <krupalbandari226@gmail.com>
2026-02-19 11:32:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 const http = require('http');
+const PORT = 3000;
 
 http.createServer((req, res) => {
-res.write("Hello from DEV Branch - NodeJS App");
-res.end();
-}).listen(3000);
-{
-  "name": "node-app",
-  "version": "1.0.0",
-  "main": "index.js"
-}
-FROM node:18
-WORKDIR /app
-COPY . .
-EXPOSE 3000
-CMD ["node", "index.js"]
-
-
+  res.write("Hello from DEV Branch - NodeJS App");
+  res.end();
+}).listen(PORT, () => console.log(`Server running on port ${PORT}`));
